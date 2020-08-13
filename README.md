25f6c7bdcbb39e4c7a48e1f8d10233bb3b8ee9ba
Mario Buikhuizen <mariobuikhuizen@gmail.com>
2020-08-13 14:43:31 +0200

    docs: add RAEDME and example

---
+++ README.md
@@ -0,0 +1,72 @@
+voila-embed-vuetify
+===================
+
+[![Version](https://img.shields.io/npm/v/voila-embed-vuetify.svg)](https://www.npmjs.com/package/voila-embed-vuetify)
+
+A [Vue](https://vuejs.org/) component to embed [ipyvuetify](https://github.com/mariobuikhuizen/ipyvuetify)
+widgets served by [Voila](https://github.com/voila-dashboards/voilà).
+
+Installation
+------------
+
+```
+npm install voila-embed-vuetify
+```
+or
+```
+yarn add voila-embed-vuetify
+```
+
+Usage
+-----
+
+```vue
+<template>
+  <div>
+    ...
+    <jupyter-widget-embed
+      voila-url="http://localhost:8000"
+      notebook="my-notebook.ipynb"
+      mount-id="my-widget"
+    ></jupyter-widget-embed>
+    ...
+  </div>
+</template>
+
+<script>
+  import { JupyterWidgetEmbed } from 'voila-embed-vuetify'
+
+  export default {
+    name: 'MyComponent',
+    components: {
+      JupyterWidgetEmbed
+    },
+    // ...
+  }
+
+</script>
+```
+
+The displayed content while loading can be replaced by specifying your own content within the
+jupyter-widget-embed tag.
+
+In your notebook set `_metadata={'mount_id': 'my-widget'}` on a ipyvuetify widget or
+`.add_traits(_metadata=traitlets.Dict(default_value={'mount_id': 'my-widget'}).tag(sync=True))` on
+any other widget.
+
+
+Example
+-------
+
+In the example directory run:
+```
+$ voila --no-browser --template=embed --enable_nbextensions=True --Voila.tornado_settings="{'allow_origin': 'http://localhost:8080'}" --port=8000
+```
+
+In another terminal in the example directory run:
+```
+$ yarn
+$ yarn serve
+```
+
+The example site is now available on http://localhost:8080
---
+++ example/src/components/Demo.vue
@@ -0,0 +1,154 @@
+<template>
+    <div>
+        <jupyter-widget-embed
+                voila-url="http://localhost:8000"
+                notebook="bqplot_vuetify_example.ipynb"
+                mount-id="content-main"
+        ></jupyter-widget-embed>
+        <jupyter-widget-embed
+                voila-url="http://localhost:8000"
+                notebook="notebook2.ipynb"
+                mount-id="dialog"
+        ></jupyter-widget-embed>
+        <jupyter-widget-embed
+                style="margin-top: 40px"
+                voila-url="http://localhost:8000"
+                notebook="bqplot_vuetify_example.ipynb"
+                mount-id="histogram_bins2"
+        ></jupyter-widget-embed>
+        <v-card v-if="sliderModel" class="ma-4 mx-auto" width="600">
+            <v-card-title class="headline">Access widget models from the page</v-card-title>
+            <v-card-text>
+                <v-row style="height: 60px">
+                    <v-col cols="3">
+                        <v-chip class="px-2">bins: {{ bins }}</v-chip>
+                    </v-col>
+                    <v-col class="flex-grow-1">
+                        <v-slider v-model="bins" hide-details></v-slider>
+                    </v-col>
+                </v-row>
+                <v-row class="align-center">
+                    <v-col cols="3">
+                        <jupyter-widget-embed
+                                voila-url="http://localhost:8000"
+                                notebook="notebook2.ipynb"
+                                mount-id="event_demo"
+                        ></jupyter-widget-embed>
+                    </v-col>
+                    <v-col class="flex-grow-1">
+                        <v-btn @click="sendClickEvent" class="mr-2">Send click event</v-btn>
+                        <v-btn @click="sendCustomEvent">Send custom event</v-btn>
+                    </v-col>
+                </v-row>
+                <v-row class="align-center">
+                    <v-col cols="3">
+                        <jupyter-widget-embed
+                                voila-url="http://localhost:8000"
+                                notebook="notebook2.ipynb"
+                                mount-id="template_event_demo"
+                        ></jupyter-widget-embed>
+                    </v-col>
+                    <v-col class="flex-grow-1">
+                        <v-btn @click="sendClickEventTemplate">Send click event (template)</v-btn>
+                    </v-col>
+                </v-row>
+            </v-card-text>
+        </v-card>
+        <v-card v-if="sliderModel" class="ma-4 mx-auto" width="600">
+            <v-card-title class="headline">Debug</v-card-title>
+            <v-card-text>
+                <jupyter-widget-embed
+                        voila-url="http://localhost:8000"
+                        notebook="notebook2.ipynb"
+                        mount-id="out"
+                ></jupyter-widget-embed>
+            </v-card-text>
+        </v-card>
+    </div>
+</template>
+
+<script>
+    import { requestWidget, JupyterWidgetEmbed } from 'voila-embed-vuetify';
+
+    export default {
+        name: "Demo",
+        components: {
+            JupyterWidgetEmbed
+        },
+        data() {
+            return {
+                sliderModel: null,
+                bins: null,
+
+                eventDemoModel: null,
+                templateEventDemoModel: null,
+            }
+        },
+        created() {
+            /* Get the widgets models. These are backbone.js models, see
+             * https://backbonejs.org/#Model for more information */
+            requestWidget({
+                voilaUrl: 'http://localhost:8000',
+                notebook: 'bqplot_vuetify_example.ipynb',
+                mountId: 'histogram_bins2',
+            }).then(sliderModel => {
+                this.sliderModel = sliderModel;
+
+                /* sync widget model changes to the vue model */
+                this.bins = sliderModel.get('v_model');
+                sliderModel.on('change:v_model', () => {
+                    this.bins = sliderModel.get('v_model')
+                });
+            });
+
+            requestWidget({
+                voilaUrl: 'http://localhost:8000',
+                notebook: 'notebook2.ipynb',
+                mountId: 'event_demo',
+            }).then(eventDemoModel => {
+                this.eventDemoModel = eventDemoModel;
+            });
+
+            requestWidget({
+                voilaUrl: 'http://localhost:8000',
+                notebook: 'notebook2.ipynb',
+                mountId: 'template_event_demo',
+            }).then(templateEventDemoModel => {
+                this.templateEventDemoModel = templateEventDemoModel;
+            });
+        },
+        watch: {
+            /* sync vue model change to the widget model */
+            bins(value) {
+                if (this.sliderModel && value !== null) {
+                    this.sliderModel.set('v_model', value);
+                    this.sliderModel.save_changes();
+                }
+            },
+        },
+        methods: {
+            sendClickEvent() {
+                this.eventDemoModel.send({
+                    event: 'click',
+                    data: {},
+                });
+            },
+            sendCustomEvent() {
+                this.eventDemoModel.send({
+                    type: 'custom',
+                    time: new Date().toISOString().substr(11, 8),
+                });
+            },
+            sendClickEventTemplate() {
+                this.templateEventDemoModel.send({
+                    event: 'my_click',
+                    data: {},
+                });
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
